4a7b51eaad38cd429a1451190c470b7935ee3757
Damon Wong <damonwcw@outlook.com>
2025-05-09 15:42:26 +0800

    PWA init

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,20 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-import App from "./App.tsx";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import App from './App.tsx';
 
 const theme = createTheme({
-  colorSchemes: {
-    dark: true
-  }
+    colorSchemes: {
+        dark: true,
+    },
 });
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-
-      <App />
-    </ThemeProvider>
-  </StrictMode>
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <App />
+        </ThemeProvider>
+    </StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,28 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      less: {
-        math: "always",
-        relativeUrls: true,
-        javascriptEnabled: true
-      }
-    }
-  }
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+            },
+            devOptions: {
+                enabled: true,
+            },
+        }),
+    ],
+    css: {
+        preprocessorOptions: {
+            less: {
+                math: 'always',
+                relativeUrls: true,
+                javascriptEnabled: true,
+            },
+        },
+    },
 });
